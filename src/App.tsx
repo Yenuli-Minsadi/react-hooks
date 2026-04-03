cc65b19da71a35217d44edad994806754c50447a
Yenuli Minsadi <samararatney@gmail.com>
2026-04-03 13:24:02 +0530

    update with redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,115 @@
-import React, { useMemo, useReducer, useState } from 'react'
-import useCounter from './hooks/useCounter';
+import { useReducer, useState } from "react";
+
+// type User = {
+//   id: string // R
+//   name: string // R
+//   email: string // R
+//   age: number // R
+//   address?: string // Optional
+// }
 
 interface User {
-  id: string
-  name: string
-  email: string
-  age: number
-  address?: string
-  
+  id: string; // R
+  name: string; // R
+  email: string; // R
+  age: number; // R
+  address?: string; // Optional
 }
 
-const  initialState = {
-  id:"",
-  name:"",
-  email:"",
-  age:0
-}
+const initialState: User = {
+  id: "",
+  name: "",
+  email: "",
+  age: 0,
+};
 
 const ACTION_TYPES = {
   SET_ID: "SET_ID",
   SET_NAME: "SET_NAME",
   SET_EMAIL: "SET_EMAIL",
-  AGE: "SET_AGE"
+  SET_AGE: "SET_AGE",
 };
 
-const reducer = (state:any, action:any) => {
+// state update here
+// state: User - currect state
+// action - action type and new data
+const reducer = (state: User, action: any) => {
   switch (action.type) {
     case ACTION_TYPES.SET_ID:
       return { ...state, id: action?.data };
     case ACTION_TYPES.SET_NAME:
       return { ...state, name: action?.data };
-    case "EMAIL":
-      ACTION_TYPES.SET_EMAIL:
+    case ACTION_TYPES.SET_EMAIL:
       return { ...state, email: action?.data };
-    case "AGE":
+    case ACTION_TYPES.SET_AGE:
       return { ...state, age: action?.data };
-      ACTION_TYPES.SET_AGE:
     default:
       return state;
   }
-
-}
+};
 
 const App = () => {
- 
-  return <div>
-    <input/>
+  const [state, dispatch] = useReducer(reducer, initialState);
+  //  const [state, dispatch] = useReducer(reducer, {
+  //   id: "",
+  //   name: "",
+  //   email: "",
+  //   age: 0
+  // })
 
-  </div>
-}
+  return (
+    <div>
+      <input
+        value={state.id}
+        onChange={(e) => {
+          dispatch(
+            // action
+            {
+              type: ACTION_TYPES.SET_ID,
+              data: e.target.value,
+            },
+          );
+        }}
+        type="text"
+        placeholder="ID"
+      />
+      <input
+        value={state.name}
+        onChange={(e) => {
+          dispatch({
+            type: ACTION_TYPES.SET_NAME,
+            data: e.target.value,
+          });
+        }}
+        type="text"
+        placeholder="Name"
+      />
+      <input
+        value={state.email}
+        onChange={(e) => {
+          dispatch({
+            type: ACTION_TYPES.SET_EMAIL,
+            data: e.target.value,
+          });
+        }}
+        type="email"
+        placeholder="Email"
+      />
+      <input
+        value={state.age}
+        onChange={(e) => {
+          dispatch({
+            type: ACTION_TYPES.SET_AGE,
+            data: e.target.value,
+          });
+        }}
+        type="number"
+        placeholder="Age"
+      />
+      <hr />
+      <pre>{JSON.stringify(state, null, 2)}</pre>
+    </div>
+  );
+};
 
-export default App
+export default App;
