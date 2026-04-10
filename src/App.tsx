147019811601effb04fe67e6fed190ad10de5aa4
Yenuli Minsadi <samararatney@gmail.com>
2026-04-10 13:17:07 +0530

    update api integration w/o catch then

--- src/App.tsx
+++ src/App.tsx
@@ -1,113 +1,41 @@
-import { useReducer, useState } from "react";
-
-// type User = {
-//   id: string // R
-//   name: string // R
-//   email: string // R
-//   age: number // R
-//   address?: string // Optional
-// }
-
-interface User {
-  id: string; // R
-  name: string; // R
-  email: string; // R
-  age: number; // R
-  address?: string; // Optional
-}
-
-const initialState: User = {
-  id: "",
-  name: "",
-  email: "",
-  age: 0,
-};
-
-const ACTION_TYPES = {
-  SET_ID: "SET_ID",
-  SET_NAME: "SET_NAME",
-  SET_EMAIL: "SET_EMAIL",
-  SET_AGE: "SET_AGE",
-};
-
-// state update here
-// state: User - currect state
-// action - action type and new data
-const reducer = (state: User, action: any) => {
-  switch (action.type) {
-    case ACTION_TYPES.SET_ID:
-      return { ...state, id: action?.data };
-    case ACTION_TYPES.SET_NAME:
-      return { ...state, name: action?.data };
-    case ACTION_TYPES.SET_EMAIL:
-      return { ...state, email: action?.data };
-    case ACTION_TYPES.SET_AGE:
-      return { ...state, age: action?.data };
-    default:
-      return state;
-  }
-};
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  //  const [state, dispatch] = useReducer(reducer, {
-  //   id: "",
-  //   name: "",
-  //   email: "",
-  //   age: 0
-  // })
+  const [customers, setCustomers] = useState([]);
 
+  //await can be only done with async keyword, only when handled as async
+  const fetchCustomer = async () => {
+    //fetch
+    //promise -> future result, obj
+    const res: any = await axios.get("http://localhost:5000/api/v1/customer"); //only can await in asynchronous function
+    console.log(res)
+    setCustomers(res.data.data);
+    // const res =  axios.get("http://localhost:5000/api/v1/customer");
+    // res
+    //   .then((result) => {
+    //     setCustomers(result.data.data)//response eka awamai callback eka run wenne
+    // })
+    // .catch((err)=>{
+    //   console.error(err)
+    // })
+  };
+
+  //mounting
+  //render
+  useEffect(() => {
+    fetchCustomer()
+  }, []);
   return (
     <div>
-      <input
-        value={state.id}
-        onChange={(e) => {
-          dispatch(
-            // action
-            {
-              type: ACTION_TYPES.SET_ID,
-              data: e.target.value,
-            },
-          );
-        }}
-        type="text"
-        placeholder="ID"
-      />
-      <input
-        value={state.name}
-        onChange={(e) => {
-          dispatch({
-            type: ACTION_TYPES.SET_NAME,
-            data: e.target.value,
-          });
-        }}
-        type="text"
-        placeholder="Name"
-      />
-      <input
-        value={state.email}
-        onChange={(e) => {
-          dispatch({
-            type: ACTION_TYPES.SET_EMAIL,
-            data: e.target.value,
-          });
-        }}
-        type="email"
-        placeholder="Email"
-      />
-      <input
-        value={state.age}
-        onChange={(e) => {
-          dispatch({
-            type: ACTION_TYPES.SET_AGE,
-            data: e.target.value,
-          });
-        }}
-        type="number"
-        placeholder="Age"
-      />
-      <hr />
-      <pre>{JSON.stringify(state, null, 2)}</pre>
+      {customers.map((customer:any) => (
+        <div>
+          <h1>Name: {customer?.name || "N/A"}</h1>
+          <p>Age: {customer?.age || "N/A"}</p>
+          <p>Admin: {customer?.isAdmin}</p>
+          <p>Email: {customer?.email || "N/A"}</p>
+        </div>
+      ))}
     </div>
   );
 };
---
+++ src/redux/components/Counter.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Counter = () => {
+  return (
+    <div>
+        <h1>Count: {count}</h1>
+        <button onClick={handleIn}></button>
+    </div>
+  )
+}
+
+export default Counter
